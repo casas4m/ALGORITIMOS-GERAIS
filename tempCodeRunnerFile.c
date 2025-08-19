79fd61d36fb7ad0abeb93d7641ab1edd1827cb82
casas4m <pedrogccasas@gmail.com>
2025-08-18 21:39:29 -0300

    tarefas do dia

---
+++ tempCodeRunnerFile.c
@@ -0,0 +1,31 @@
+#include <studio.h>
+
+int main(){
+    int valor_b;
+int valor_C;
+int soma;
+
+print =f("Digite o valor a:");
+scanf("%d",&valor_a);
+
+print =f("Digite o valor b:");
+scanf("%d",&valor_b);
+
+print =f("Digite o valor c:");
+scanf("%d",&valor_c);
+
+soma = valor_a + valor_b + valor_c;
+
+//Saida do valor
+printf("digite o valor a;");
+sacnf ("&d", &valor_a);
+
+printf("digite o valor b;");
+sacnf ("&d", &valor_b)
+
+printf("digite o valor c;");
+sacnf ("&d", &valor_c);
+return 0;
+
+}
+
